9a3276d1e2eafcc476a49fa5f784595d2f9b09ab
Chinmay Chhajed <chhajedchinmay@gmail.com>
2024-03-25 12:10:03 -0400

    Moved all files to PDFs folder.

---
+++ PDFs/rename.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+
+def rename_files(dir_path):
+  for filename in os.listdir(dir_path):
+    file_name, extension = os.path.splitext(filename)
+    new_name = file_name.replace(" ", "_")
+    new_filename = f"{new_name}{extension}"
+
+    old_path = os.path.join(dir_path, filename)
+    new_path = os.path.join(dir_path, new_filename)
+
+    if old_path != new_path:
+      os.rename(old_path, new_path)
+
+rename_files(os.getcwd())
+
+print("Finished renaming files.")
